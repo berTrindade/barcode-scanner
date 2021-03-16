210b0d83b700b369cdb78928838782f8f148cbe5
Bernardo Trindade de Abreu <bernardo-trindade@hotmail.com>
2021-03-16 07:29:17 -0300

    Add react-native-camera

--- .history/App_20210315224827.js
+++ .history/App_20210315224827.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello!</Text>
+      <Text>Hello Bernardo!</Text>
       <StatusBar style="auto" />
     </View>
   );
